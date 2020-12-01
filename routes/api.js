6046d1697b913386febcb47e781b4e6ca633317c
Oliver Brown <oliverbrodev@gmail.com>
2020-12-01 04:33:55 -0400

    actualizacion de modulos

--- routes/api.js
+++ routes/api.js
@@ -3,6 +3,7 @@ const router = express.Router()
 const User = require('./models/user')
 const mongoose = require('mongoose')
 const db = "mongodb+srv://user:pass4789@cluster0.bwdux.mongodb.net/eventsdb?retryWrites=true&w=majority" 
+// const db = "mongodb+srv://user:<password>@cluster0.bwdux.mongodb.net/<dbname>?retryWrites=true&w=majority"
 
 mongoose.connect(db, err => {
     if(err) {
